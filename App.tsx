b468bb4256998f63d3ad180df1d8eb0eb0c52881
Koushik <koushikroy.jis@gmail.com>
2023-07-18 17:00:32 +1000

    Input field

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!"></TextInput>
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals ...</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "red",
+    padding: 16,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderStyle:'solid',
+    borderColor:'red',
+    borderWidth:2
   },
 });
